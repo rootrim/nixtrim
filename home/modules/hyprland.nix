8b32f1291f8ed71af1f875e9107dd28d060d7bba
rootrim <omuaz01@gmail.com>
2025-03-02 16:15:30 +0300

    fix

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,8 +1,13 @@
 {
+  inputs,
+  pkgs,
+  ...
+}:
+{
   wayland.windowManager.hyprland = {
     enable = true;
-    package = null;
-    #package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    #package = null;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     #portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
     systemd.enable = false;
