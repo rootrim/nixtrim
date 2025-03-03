7eddb8bd8f29868f1f1f5b53b7bd38b7550e5aac
rootrim <omuaz01@gmail.com>
2025-03-03 12:47:56 +0300

    fix

---
+++ hosts/modules/services.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  services = {
+    printing.enable = true;
+    openssh.enable = true;
+    hypridle.enable = true;
+    dbus.enable = true;
+    xserver = {
+      enable = false;
+      videoDrivers = ["nvidia"];
+      xkb = {
+        layout = "tr";
+        variant = "";
+      };
+    };
+    libinput.enable = true;
+    displayManager = {
+      ly = {
+        enable = true;
+        package = pkgs.ly;
+      };
+    };
+    gnome.gnome-keyring = {
+      enable = true;
+      components = [
+        "pkcs11"
+        "secrets"
+        "ssh"
+      ];
+    };
+  };
+}
