e69530325ced9309b21420dc23a370c163be8257
rootrim <omuaz01@gmail.com>
2025-03-09 19:02:44 +0300

    new cachixes

--- hosts/modules/nixsettings.nix
+++ hosts/modules/nixsettings.nix
@@ -2,14 +2,20 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
+      "https://cache.nixos.org?priority=10"
+      "https://anyrun.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://cuda-maintainers.cachix.org"
+      "https://nix-gaming.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
   home-manager.backupFileExtension = "backup";
