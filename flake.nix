abba1f976c489ea308442233b152929e80192246
rootrim <omuaz01@gmail.com>
2025-03-16 12:53:44 +0300

    appts

--- flake.nix
+++ flake.nix
@@ -37,7 +37,31 @@
     hostname = "zenith";
     username = "rootrim";
     system  = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
+
+    packages.${system}.deskshell = pkgs.stdenvNoCC.mkDerivation rec {
+      name = "deskshell";
+      src = ./dots/astal;
+
+      nativeBuildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
+      ];
+
+      buildInputs = with astal.packages.${system}; [
+        astal3
+        io
+        # any other package
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        ags bundle app.ts $out/bin/${name}
+      '';
+    };
+
     nixosConfigurations = {
       "${hostname}" = nixpkgs.lib.nixosSystem {
         specialArgs = {
