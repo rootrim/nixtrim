c14ce9ff769895fb0e31d1f1d0989b07a267f8c6
rootrim <omuaz01@gmail.com>
2025-04-20 17:22:38 +0300

    nani

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,18 +1,19 @@
 {
-    imports = [
-      ./git.nix
-      ./spicetify.nix
-      ./hyprland.nix
-      ./waybar.nix
-      ./rofi.nix
-      ./sh.nix
-      ./starship.nix
-      ./qt.nix
-      ./tmux.nix
-      ./hypridle.nix
-      ./hyprlock.nix
-      ./zoxide.nix
-      ./stylix.nix
-      ./ghostty.nix
-    ];
+  imports = [
+    ./git.nix
+    ./spicetify.nix
+    ./hyprland.nix
+    ./waybar.nix
+    ./rofi.nix
+    ./sh.nix
+    ./starship.nix
+    ./qt.nix
+    ./tmux.nix
+    ./hypridle.nix
+    ./hyprlock.nix
+    ./zoxide.nix
+    ./stylix.nix
+    ./ghostty.nix
+    ./sherlock.nix
+  ];
 }
