252cb379b1d9cd4257c0a8183cf79578293a49a1
rootrim <omuaz01@gmail.com>
2025-05-16 18:19:30 +0300

    ld

--- hosts/modules/nix-ld.nix
+++ hosts/modules/nix-ld.nix
@@ -1,15 +1,16 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   programs.nix-ld = {
-    enable = false;
+    enable = true;
     libraries = with pkgs; [
+      libGL
       xorg.libXxf86vm
       xorg.libXext
       xorg.libX11
       glib
+      zlib
+      alsa-lib
+      libxkbcommon
     ];
   };
 }
