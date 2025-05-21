448b5a1bf9d7c13407c3449800ab01ba27e88979
rootrim <omuaz01@gmail.com>
2025-05-21 22:03:03 +0300

    finally

--- home/modules/mpd.nix
+++ home/modules/mpd.nix
@@ -1,5 +1,17 @@
 {
+  username,
+  ...
+}:
+
+let
+  musicDir = "/home/${username}/Music";
+in
+
+{
   home.file.".config/mpd/mpd.conf".source = ../../dots/mpd/mpd.conf;
 
-  services.mpd.enable = true;
+  services.mpd = {
+    musicDirectory = musicDir;
+    enable = true;
+  };
 }
--- hosts/modules/mpd.nix
+++ hosts/modules/mpd.nix
@@ -3,9 +3,13 @@
   ...
 }:
 
+let
+  musicDir = "/home/${username}/Music";
+in
 {
   services.mpd = {
     enable = true;
     user = username;
+    musicDirectory = musicDir;
   };
 }
