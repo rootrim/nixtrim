40e3157f62c5a58b4c5620b46c746645983cfcb8
rootrim <omuaz01@gmail.com>
2025-05-21 22:00:00 +0300

    fixesd

--- home/modules/mpd.nix
+++ home/modules/mpd.nix
@@ -1,3 +1,5 @@
 {
-  home.file.".config/mpd/mpd.conf".source = ../../mpd/mpd.conf;
+  home.file.".config/mpd/mpd.conf".source = ../../dots/mpd/mpd.conf;
+
+  services.mpd.enable = true;
 }
--- hosts/modules/mpd.nix
+++ hosts/modules/mpd.nix
@@ -1,28 +1,11 @@
 {
-  config,
-  pkgs,
   username,
   ...
 }:
 
-let
-  musicDir = "/home/${username}/Music";
-  mpdDir = "/home/${username}/.config/mpd";
-in
 {
   services.mpd = {
     enable = true;
     user = username;
-    musicDirectory = musicDir;
-    dbFile = "${mpdDir}/database";
-    playlistDirectory = "${mpdDir}/playlists";
-    extraConfig = ''
-      state_file "/home/${username}/.config/mpd/state"
-
-      audio_output {
-        type "pipewire"
-        name "Pipewire Output"
-      }
-    '';
   };
 }
