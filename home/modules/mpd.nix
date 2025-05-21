7dba94e56abfa9ab476752a08d1b2628eee38238
rootrim <omuaz01@gmail.com>
2025-05-21 21:55:12 +0300

    strange isnt it

--- home/modules/mpd.nix
+++ home/modules/mpd.nix
@@ -1,24 +1,3 @@
 {
-  username,
-  ...
-}:
-
-let
-  musicDir = "/home/${username}/Music";
-  mpdDir = "/home/${username}/.config/mpd";
-in
-{
-  services.mpd = {
-    enable = true;
-    musicDirectory = musicDir;
-    dataDir = mpdDir;
-    dbFile = "${mpdDir}/database";
-    playlistDirectory = "${mpdDir}/playlists";
-    extraConfig = ''
-      audio_output {
-        type "pipewire"
-        name "My PipeWire Output"
-      }
-    '';
-  };
+  home.file.".config/mpd/mpd.conf".source = ../../mpd/mpd.conf;
 }
