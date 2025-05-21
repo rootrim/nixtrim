b5b9a3bda62e93bb11d4f6443eeb54b521775ffa
rootrim <omuaz01@gmail.com>
2025-05-21 20:39:07 +0300

    update

--- home/modules/mpd.nix
+++ home/modules/mpd.nix
@@ -1,13 +1,30 @@
-{ username, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+
+let
+  musicDir = "/home/${username}/Music";
+  mpdDir = "/home/${username}/.config/mpd";
+in
 {
   services.mpd = {
     enable = true;
-    musicDirectory = "/home/${username}/Music";
+    musicDirectory = musicDir;
     extraConfig = ''
+      db_file "${mpdDir}/database"
+      state_file "${mpdDir}/state"
+      playlist_directory "${mpdDir}/playlists"
+
       audio_output {
         type "pipewire"
         name "My PipeWire Output"
       }
     '';
   };
+
+  users.users.${username}.extraGroups = [ "audio" ];
 }
