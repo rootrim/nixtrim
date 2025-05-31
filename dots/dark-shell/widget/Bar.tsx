1679900fdd1c7895ac914227d993226f8c941ec3
rootrim <omuaz01@gmail.com>
2025-05-31 18:53:36 +0300

    formatted ags

--- dots/dark-shell/widget/Bar.tsx
+++ dots/dark-shell/widget/Bar.tsx
@@ -4,33 +4,33 @@ import { Variable } from "astal"
 const time = Variable("").poll(1000, "date")
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
-    return <window
-        visible
-        cssClasses={["Bar"]}
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
-        application={App}>
-        <centerbox cssName="centerbox">
-            <button
-                onClicked="echo hello"
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                Welcome to AGS!
-            </button>
-            <box />
-            <menubutton
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                <label label={time()} />
-                <popover>
-                    <Gtk.Calendar />
-                </popover>
-            </menubutton>
-        </centerbox>
-    </window>
+  return <window
+    visible
+    cssClasses={["Bar"]}
+    gdkmonitor={gdkmonitor}
+    exclusivity={Astal.Exclusivity.EXCLUSIVE}
+    anchor={TOP | LEFT | RIGHT}
+    application={App}>
+    <centerbox cssName="centerbox">
+      <button
+        onClicked="echo hello"
+        hexpand
+        halign={Gtk.Align.CENTER}
+      >
+        Welcome to AGS!
+      </button>
+      <box />
+      <menubutton
+        hexpand
+        halign={Gtk.Align.CENTER}
+      >
+        <label label={time()} />
+        <popover>
+          <Gtk.Calendar />
+        </popover>
+      </menubutton>
+    </centerbox>
+  </window>
 }
