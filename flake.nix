f56dfcee2dffb2886a159901afb8f3a616ac1f91
rootrim <omuaz01@gmail.com>
2025-06-30 12:46:38 +0300

    removed spicetify
    I don't use spotify anymore

--- flake.nix
+++ flake.nix
@@ -25,7 +25,6 @@
     };
     ags.url = "github:aylur/ags";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     yazi.url = "github:sxyazi/yazi";
   };
 
