4f3a435ec188759e2ea54ef55a20480115761b8e
rootrim <omuaz01@gmail.com>
2025-08-27 16:45:45 +0300

    update

---
+++ hosts/modules/nh.nix
@@ -0,0 +1,9 @@
+{ hostname, ... }: {
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake =
+      "/home/${hostname}/.config/nixtrim/"; # sets NH_OS_FLAKE variable for you
+  };
+}
