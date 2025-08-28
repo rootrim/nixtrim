fe694e914ae2639e58cfbb679a56fd1746462a95
rootrim <omuaz01@gmail.com>
2025-08-28 20:48:57 +0300

    not use os prober

--- hosts/modules/bootloader.nix
+++ hosts/modules/bootloader.nix
@@ -5,7 +5,7 @@
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.useOSProber = false;
 
   #boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
   #  pname = "distro-grub-themes";
