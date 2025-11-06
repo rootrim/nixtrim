f1238a29db0fac74f4568b45bb032b827e0369fe
rootrim <omuaz01@gmail.com>
2025-11-06 23:29:48 +0300

    fixed issues

--- home/modules/de/services/hyprlock/hyprlock.nix
+++ home/modules/de/services/hyprlock/hyprlock.nix
@@ -3,24 +3,24 @@ let
   layer1 = "./layer1.png";
   layer2 = "./layer2.png";
 
-  background = "282828";
-  foreground = "ebdbb2";
-  color0 = "3c3836";
-  color1 = "cc241d";
-  color2 = "98971a";
-  color3 = "d79921";
-  color4 = "458588";
-  color5 = "b16286";
-  color6 = "689d6a";
-  color7 = "a89984";
-  color8 = "928374";
-  color9 = "fb4934";
-  color10 = "b8bb26";
-  color11 = "fabd2f";
-  color12 = "83a598";
-  color13 = "d3869b";
-  color14 = "8ec07c";
-  color15 = "fbf1c7";
+  background = "rgb(282828)";
+  foreground = "rgb(ebdbb2)";
+  color0 = "rgb(3c3836)";
+  color1 = "rgb(cc241d)";
+  color2 = "rgb(98971a)";
+  color3 = "rgb(d79921)";
+  color4 = "rgb(458588)";
+  color5 = "rgb(b16286)";
+  color6 = "rgb(689d6a)";
+  color7 = "rgb(a89984)";
+  color8 = "rgb(928374)";
+  color9 = "rgb(fb4934)";
+  color10 = "rgb(b8bb26)";
+  color11 = "rgb(fabd2f)";
+  color12 = "rgb(83a598)";
+  color13 = "rgb(d3869b)";
+  color14 = "rgb(8ec07c)";
+  color15 = "rgb(fbf1c7)";
 
   font = "Caskaydia Cove Nerd Font Mono";
 
@@ -30,12 +30,8 @@ in {
 
     settings = {
       general = {
-        disable_loading_bar = true;
         immediate_render = true;
         hide_cursor = true;
-        no_fade_in = true;
-        no_fade_out = true;
-        grace = 0;
       };
 
       background = [
@@ -46,23 +42,19 @@ in {
         {
           monitor = "";
           zindex = 1;
-          keep_aspect_ratio = true;
-          rounding = 0;
-          border_size = 0;
           path = layer1;
         }
         {
           monitor = "";
           zindex = 2;
-          keep_aspect_ratio = true;
-          rounding = 0;
-          border_size = 0;
           path = layer2;
         }
       ];
 
       input-field = [{
         monitor = "";
+
+        zindex = 2;
 
         size = "300, 50";
         valign = "center";
@@ -84,7 +76,6 @@ in {
 
         dots_spacing = 0.2;
         dots_center = true;
-        dots_fade_time = 100;
       }];
 
       label = [
