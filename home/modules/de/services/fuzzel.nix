697891f272b87d38415938a81e5b5de23e8619bd
rootrim <omuaz01@gmail.com>
2025-11-14 19:32:17 +0300

    changes on fuzzel

--- home/modules/de/services/fuzzel.nix
+++ home/modules/de/services/fuzzel.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }: {
+{
   programs.fuzzel = {
     enable = true;
-
     settings = {
       main = {
-        terminal = "${pkgs.kitty}/bin/kitty";
+        terminal = "kitty -e";
+        width = 40;
+        line-height = 28;
+        show-actions = "yes";
+        exit-on-keyboard-focus-loss = "yes";
         layer = "overlay";
+        lines = 6;
+        minimal-lines = true;
+        x-margin = 10;
+        y-margin = 10;
+        icons-enabled = true;
+      };
+      border = {
+        width = 3;
+        radius = 10;
       };
     };
   };
