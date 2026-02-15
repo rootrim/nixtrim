65c7c3d683a988c31a73e8f621bc362077916f0c
rootrim <omuaz01@gmail.com>
2026-02-15 16:39:53 +0300

    not latest anymore

--- hosts/zenith/hardware-configuration.nix
+++ hosts/zenith/hardware-configuration.nix
@@ -7,7 +7,7 @@
     [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
