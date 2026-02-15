cf8aa3b25e14e62be532e74f355497b7219c1699
rootrim <omuaz01@gmail.com>
2026-02-15 21:11:26 +0300

    picom config

--- home/modules/de/services/picom.nix
+++ home/modules/de/services/picom.nix
@@ -1,1 +1,32 @@
-{ services.picom = { enable = true; }; }
+{
+  services.picom = {
+    enable = true;
+    backend = "glx";
+    vSync = true;
+
+    shadow = true;
+    shadowOffsets = [ 4 4 ];
+    shadowOpacity = 0.7;
+
+    fade = true;
+    fadeDelta = 3;
+    fadeSteps = [ 3.0e-2 3.0e-2 ];
+
+    inactiveOpacity = 0.9;
+
+    extraConfig = ''
+      shadow-radius = 1
+      corner-radius = 0
+
+      blur: 
+      {
+        method = "dual_kawase";
+        size = 10;
+        strength = 3;
+      };
+
+      blur-background = true;
+      blur-background-fixed = true;
+    '';
+  };
+}
