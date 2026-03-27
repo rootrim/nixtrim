d0831bd7b60a546b5c13d861c2624dee0ae7175b
rootrim <omuaz01@gmail.com>
2026-03-27 21:25:16 +0300

    fahh

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,8 +1,11 @@
-{
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     localNetworkGameTransfers.openFirewall = true;
     extest.enable = true;
     remotePlay.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 }
