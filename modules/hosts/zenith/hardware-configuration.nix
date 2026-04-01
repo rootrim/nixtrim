633d9a5353575ee2d5512a5090e4576387a35aa2
rootrim <omuaz01@gmail.com>
2026-04-01 17:42:22 +0300

    added ntsync

--- modules/hosts/zenith/hardware-configuration.nix
+++ modules/hosts/zenith/hardware-configuration.nix
@@ -9,7 +9,7 @@
 
     boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod"];
     boot.initrd.kernelModules = [];
-    boot.kernelModules = ["kvm-intel"];
+    boot.kernelModules = ["kvm-intel" "ntsync"];
     boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto;
     boot.kernelParams = ["loglevel=0" "quiet" "rd.systemd.show_status=false" "rd.udev.log_level=0" "udev.log_level=0"];
     boot.extraModulePackages = [];
