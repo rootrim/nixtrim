e77acf51fe482ad8343107c89bf9f3aab3d7a38d
rootrim <omuaz01@gmail.com>
2026-04-02 20:46:10 +0300

    added homeModules to imports

--- modules/hosts/zenith/home-configuration.nix
+++ modules/hosts/zenith/home-configuration.nix
@@ -4,8 +4,38 @@
     home-manager.useUserPackages = true;
     home-manager.users.rootrim = {
       imports = [
-        self.homeModules.rootrimHome
+        self.homeModules.bacon
+        self.homeModules.btop
+        self.homeModules.delta
+        self.homeModules.fastfetch
+        self.homeModules.fish
+        self.homeModules.fsel
+        self.homeModules.git
+        self.homeModules.gtk
+        self.homeModules.helix
         self.homeModules.homePackages
+        self.homeModules.hypridle
+        self.homeModules.hyprland
+        self.homeModules.hyprlock
+        self.homeModules.hyprpaper
+        self.homeModules.jujutsu
+        self.homeModules.kitty
+        self.homeModules.lazygit
+        self.homeModules.mako
+        self.homeModules.mangohud
+        self.homeModules.nix-your-shell
+        self.homeModules.picom
+        self.homeModules.qt
+        self.homeModules.quickshell
+        self.homeModules.qutebrowser
+        self.homeModules.rofi
+        self.homeModules.rootrimHome
+        self.homeModules.starship
+        self.homeModules.stylix
+        self.homeModules.televison
+        self.homeModules.yazi
+        self.homeModules.zellij
+        self.homeModules.zoxide
       ];
     };
   };
