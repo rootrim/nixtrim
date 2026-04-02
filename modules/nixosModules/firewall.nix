440f1bae77b44fa4679c8c335ba31beb03b248fa
rootrim <omuaz01@gmail.com>
2026-04-02 22:06:53 +0300

    added proxychain

--- modules/nixosModules/firewall.nix
+++ modules/nixosModules/firewall.nix
@@ -1,9 +1,20 @@
 {
   flake.nixosModules.firewall = {
     programs.firejail.enable = true;
+
     networking.firewall = {
       enable = true;
       allowedTCPPorts = [22];
+    };
+
+    programs.proxychains = {
+      enable = true;
+      proxies.spoofdpi = {
+        enable = true;
+        type = "http";
+        host = "127.0.0.1";
+        port = 8080;
+      };
     };
   };
 }
