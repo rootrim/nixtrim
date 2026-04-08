5ae9af8fee6dff26b0d000822118a7a953d1934d
rootrim <omuaz01@gmail.com>
2026-04-08 23:32:10 +0300

    added lantian trusted key

--- modules/nixosModules/nixsettings.nix
+++ modules/nixosModules/nixsettings.nix
@@ -17,6 +17,7 @@
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "unmojang.cachix.org-1:OfHnbBNduZ6Smx9oNbLFbYyvOWSoxb2uPcnXPj4EDQY="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         ];
       };
     };
