1a184fa5881e9beb9a9a24cfcd81fb868175ed6b
rootrim <omuaz01@gmail.com>
2026-05-28 22:29:54 +0300

    wallpapers from heaven's door

--- modules/homeModules/hyprland/hyprpaper.nix
+++ modules/homeModules/hyprland/hyprpaper.nix
@@ -1,6 +1,9 @@
 {
-  flake.homeModules.hyprpaper = let
-    wall = ./groovyspacebox.png;
+  flake.homeModules.hyprpaper = {pkgs, ...}: let
+    wall = pkgs.fetchurl {
+      url = "https://w.wallhaven.cc/full/x1/wallhaven-x13qzz.jpg";
+      sha256 = "1flb25lyxmcmvmmb22d5xx6djik4jjh48bp2ad286dza3gcs7v4r";
+    };
   in {
     services.hyprpaper = {
       enable = true;
