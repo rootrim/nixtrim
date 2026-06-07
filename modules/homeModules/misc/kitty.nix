c82fae2c8250d77a03e97d85e47247ebedbe4d5d
rootrim <omuaz01@gmail.com>
2026-06-07 11:27:02 +0300

    smol menu

--- modules/homeModules/misc/kitty.nix
+++ modules/homeModules/misc/kitty.nix
@@ -16,9 +16,12 @@
       quickAccessTerminalConfig = {
         start_as_hidden = false;
         hide_on_focus_loss = false;
-        background_opacity = 1;
-        column = 30;
-        edge = "left";
+        background_opacity = 1.0;
+        margin_left = 40;
+        margin_right = 5;
+        margin_top = 40;
+        lines = 15;
+        edge = "top";
       };
     };
   };
