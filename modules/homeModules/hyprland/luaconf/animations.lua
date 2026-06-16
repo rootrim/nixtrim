a9d8ac3a2f56edcdf896e79003d83fbda90f2652
rootrim <omuaz01@gmail.com>
2026-06-16 18:58:36 +0300

    update

--- modules/homeModules/hyprland/luaconf/animations.lua
+++ modules/homeModules/hyprland/luaconf/animations.lua
@@ -5,14 +5,12 @@ hl.config({
 })
 
 hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve("rubber", { type = "spring", mass = 1, stiffness = 70, dampening = 10 })
-hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
 hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
 
 hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
-hl.animation({ leaf = "windows", enabled = true, speed = 2, spring = "rubber", style = "gnomed" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.5, spring = "easy", style = "gnomed" })
+hl.animation({ leaf = "windows", enabled = true, speed = 1.6, bezier = "easeOutQuint", style = "gnomed" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 0.9, bezier = "easeOutQuint", style = "gnomed" })
 hl.animation({ leaf = "workspaces", enabled = true, speed = 3, bezier = "easeOutQuint", style = "slidefadevert" })
 hl.animation({ leaf = "specialWorkspace", enabled = true, speed = 3, bezier = "easeOutQuint", style = "slidevert" })
 hl.animation({ leaf = "border", enabled = true, speed = 3, bezier = "easeOutQuint" })
