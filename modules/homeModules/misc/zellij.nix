386cde4b677738fff10db1caadf0cc060d937b37
rootrim <omuaz01@gmail.com>
2026-07-18 18:38:31 +0300

    new zellij config

--- modules/homeModules/misc/zellij.nix
+++ modules/homeModules/misc/zellij.nix
@@ -2,7 +2,16 @@
   flake.homeModules.zellij = {
     programs.zellij = {
       enable = true;
-      enableFishIntegration = false;
+      enableFishIntegration = true;
+
+      settings = {
+        theme = "gruvbox-dark";
+        pane_frames = false;
+        simplified_ui = true;
+        mouse_mode = false;
+        show_startup_tips = false;
+        default_layout = "compact";
+      };
     };
   };
 }
